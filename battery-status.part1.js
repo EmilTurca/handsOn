47288005cdc070f92a21f63dbc7efeb4ce9ca1ef
EmilTurca <152076415+EmilTurca@users.noreply.github.com>
2024-03-12 00:08:04 +0200

    Create battery-status.part1.js

---
+++ battery-status.part1.js
@@ -0,0 +1,8 @@
+baterryCharge = 70
+
+Full_BATTERY = 100
+chargePercentInMinutes = 10
+chargePercentInHours = 60
+
+etWorkInMinutes = baterryCharge * chargePercentInMinutes
+etWorkInHours = etWorkInMinutes / chargePercentInHours
